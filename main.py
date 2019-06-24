8318a0223ed188851bbe18908cd34b5bd6f4772c
CROW18BRG <a12634@alunos.ipca.pt>
2019-06-24 20:03:40 +0100

    v1.0

---
+++ main.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="hd5gcUpDk",
+ database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute("create table sala (id int AUTO_INCREMENT,f1 VARCHAR(255),PRIMARY KEY(id)); ")
+#mycursor.execute("show columns FROM sala")
+
+#mycursor.execute("CREATE TABLE quarto (name VARCHAR(255), f1 VARCHAR(255)),f2")
+#
+#sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+#val = ("musica4", "asa5adada6878787a7")
+#mycursor.execute(sql, val)
+
+#mydb.commit()
+#
+# print(mycursor.rowcount, "record inserted.")
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
